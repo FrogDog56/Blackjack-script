922f2f901f986814c1ee6f60cfa8b4b82f37f193
FrogDog56 <83395400+FrogDog56@users.noreply.github.com>
2022-04-08 21:03:51 +0100

    card values

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-from calendar import c
-import random
-import string
-from traceback import print_last
+import random as r
 
 cardTypes = ("hearts", "clubs", "spades", "diamonds")
-cardValues = ("ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king")
+cardNames = ("ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king")
+cardValues = {
+                "ace" : 1, 
+                "2" : 2, 
+                "3" : 3, 
+                "4" : 4,
+                "5" : 5,
+                "6" : 6,
+                "7" : 7,
+                "8" : 8,
+                "9" : 9,
+                "10" : 10,
+                "jack" : 10,
+                "queen" : 10,
+                "king" : 10,
+                }
 
 cards = []
+
+dealerHand = []
+playerHand = []
 
 class Card(object):
     def __init__(self, type, value):
@@ -16,7 +31,10 @@ class Card(object):
 def addDeck():
     for x in range(0, len(cardTypes)):
         for y in range(0, len(cardValues)):
-            cards.append(Card(cardTypes[x], cardValues[y]))
+            cards.append(Card(cardTypes[x], cardNames[y]))
+
+def total(hand):
+    return cardValues.get(hand[0].value) + cardValues.get(hand[1].value)
 
 def init(decks):
     for x in range(0, int(decks)):
@@ -36,7 +54,13 @@ def main():
     else:
         pass
 
+    for x in range(0, 2):
+        randomCard = cards[r.randint(0, len(cards))]
+        dealerHand.append(randomCard)
 
+    for x in range(0, len(dealerHand)):
+        print(f"The dealer has {dealerHand[x].type} {dealerHand[x].value}")
+        print(total(dealerHand))
 
 if __name__ == "__main__":
     main()
